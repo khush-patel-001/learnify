e14c644ec904da4e0b375e426ffd034335bd5e55
khush-patel-001 <khushpatel0030@gmail.com>
2024-12-31 19:08:47 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/learnify">
+    <BrowserRouter>
       <Routes>
         <Route path="/learnify" element={<App />} />
       </Routes>
