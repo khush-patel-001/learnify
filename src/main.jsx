6d4fd58ae337a485e98b6988e684a8c8ca8b6ed3
khush-patel-001 <khushpatel0030@gmail.com>
2024-12-31 19:18:18 +0530

    Prepare for github pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
+import {Routes, Route, HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/learnify" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
